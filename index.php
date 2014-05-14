16b0373b259042e1537f01346d876456cc72cc18
OSN <oluwadabira@gmail.com>
2014-05-14 12:49:20 +1000

    start commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Blashshshs</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<p>sadasdasd</p>
+	<h1>asdasdsad</h1>
+	
+</body>
+</html>
